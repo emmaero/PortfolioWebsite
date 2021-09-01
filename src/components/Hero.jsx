168a30b0f28af2cb61958bbe8f5307c4e3612bb2
Emmanuel Imhontu <imhontu.emmanuel@yahoo.com>
2021-09-01 15:13:42 +0200

    hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Hero() {
+    return (
+        <section className="hero" id="hero">
+            <h1>Hi i’m Emmanuel a frontend developer based in Halmstad, Sweden</h1>
+        </section>
+    )
+}
